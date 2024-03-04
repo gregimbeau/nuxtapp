86c39bf7180061d85ffadb00f72b3fd85103d601
Guillaume Regimbeau <gregimbeau@mail.com>
2024-03-04 12:31:08 +0100

    logginIn

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -1,23 +1,43 @@
 <template>
   <div class="flex justify-center items-center h-full">
-    <form @submit.prevent="handleLogin" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <form
+      @submit.prevent="handleLogin"
+      class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
       <div class="mb-4">
-        <label for="username" class="block text-gray-700 text-sm font-bold mb-2">Nom d'utilisateur:</label>
-        <input id="username" v-model="credentials.username" type="text" required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+        <label for="username" class="block text-gray-700 text-sm font-bold mb-2"
+          >Nom d'utilisateur:</label
+        >
+        <input
+          id="username"
+          v-model="credentials.username"
+          type="text"
+          required
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
       </div>
       <div class="mb-6">
-        <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Mot de passe:</label>
-        <input id="password" v-model="credentials.password" type="password" required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline">
+        <label for="password" class="block text-gray-700 text-sm font-bold mb-2"
+          >Mot de passe:</label
+        >
+        <input
+          id="password"
+          v-model="credentials.password"
+          type="password"
+          required
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
       </div>
-      <div class="flex items-center justify-between">
-        <button type="submit" class="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-          Se connecter
+     <div class="flex items-center justify-between">
+        <button
+          type="submit"
+          :class="{'bg-blue-500': !isLoggingIn, 'bg-green-500': isLoggingIn}"
+          class="relative overflow-hidden text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition-colors duration-500 ease-in-out">
+          <span class="block" :class="{'swipe-effect': isLoggingIn}">
+            {{ loginButtonText }}
+          </span>
         </button>
       </div>
     </form>
   </div>
 </template>
-
 
 <script setup>
 import { ref } from "vue";
@@ -25,15 +45,38 @@ import { useAuth } from "@/composables/useAuth";
 
 const { login, checkAuthStatus } = useAuth();
 const credentials = ref({ username: "", password: "" });
+const loginButtonText = ref("Se connecter");
+const isLoggingIn = ref(false);
 
-// Appeler checkAuthStatus au chargement du composant
 checkAuthStatus();
 
 const handleLogin = async () => {
-  await login(credentials.value);
+  isLoggingIn.value = true;
+  loginButtonText.value = "Logging in...";
+
+  setTimeout(async () => {
+    await login(credentials.value);
+    isLoggingIn.value = false;
+    loginButtonText.value = "Se connecter";
+    // Add your redirection logic here, if any
+  }, 2000);
 };
 </script>
 
 <style scoped>
-/* Ajoutez ici des styles supplémentaires si nécessaire */
+.swipe-effect {
+  position: relative;
+  animation: swipe 2s forwards;
+}
+
+@keyframes swipe {
+  0% {
+    transform: translateX(-100%);
+  }
+  100% {
+    transform: translateX(0%);
+  }
+}
+
+/* You can add more styles here if necessary */
 </style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,23 +1,41 @@
 <template>
   <div class="flex h-screen">
     <!-- Left Side -->
-    <div class="w-1/2 bg-cover bg-center" style="background-image: url('https://source.unsplash.com/random')">
-      <div class="flex h-full bg-gray-900 bg-opacity-50 justify-center items-center">
+    <div
+      class="w-1/2 bg-cover bg-center"
+      style="background-image: url('https://source.unsplash.com/random')">
+      <div
+        class="flex h-full bg-gray-900 bg-opacity-50 justify-center items-center">
         <div class="text-white max-w-md mx-4 text-center">
-          <h2 class="text-3xl font-semibold">Welcome to our connection interface.</h2>
-          <p class="mt-4">Please enter your credentials on the right. If you do not have any, please mail us to request account creation.</p>
+          <h2 class="text-3xl font-semibold">
+            Welcome to our connection interface.
+          </h2>
+          <p class="mt-4">
+            Please enter your credentials on the right. If you do not have any,
+            please mail us to request account creation.
+          </p>
         </div>
       </div>
     </div>
-    
+
     <!-- Right Side -->
-    <div class="w-1/2 flex flex-col justify-center items-center bg-gray-100 space-y-4">
+    <div
+      class="w-1/2 flex flex-col justify-center items-center bg-gray-100 space-y-4">
       <div class="w-full max-w-md">
-        <LoginForm />
+        <LoginForm
+          @login-start="handleLoginStart"
+          @login-end="handleLoginEnd" />
+
+        <div
+          v-if="loading"
+          class="absolute inset-0 flex justify-center items-center bg-black bg-opacity-50">
+          <span class="text-white">Loading, please wait...</span>
+        </div>
       </div>
-      <pre class="text-green-500 w-2/6 flex flex-col justify-center items-center bg-black space-y-4">
+      <pre
+        class="text-green-500 w-2/6 flex flex-col justify-center items-center bg-black space-y-4">
         <code>
-//compte de test
+// Test account
 
 {
   "username": "testUser2",
@@ -26,11 +44,29 @@
         </code>
       </pre>
     </div>
-    
   </div>
 </template>
 
-
 <script setup>
-import LoginForm from '../components/LoginForm.vue';
+import { ref } from "vue";
+import LoginForm from "../components/LoginForm.vue";
+
+const loading = ref(false);
+
+let loginStartTime = 0;
+
+const handleLoginStart = () => {
+  loading.value = true;
+  loginStartTime = Date.now(); // Record the start time
+};
+
+const handleLoginEnd = () => {
+  const elapsedTime = Date.now() - loginStartTime;
+  const remainingTime = Math.max(0, 2000 - elapsedTime);
+
+  // Wait for the remainder of the 2 seconds, if necessary
+  setTimeout(() => {
+    loading.value = false;
+  }, remainingTime);
+};
 </script>
